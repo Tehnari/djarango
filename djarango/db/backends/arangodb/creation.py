a65a1efa39e65c8f3ac06f6d9a1c19c494028ef9
AdrianGarcia-StayWithReside <adrian.garcia@staywithreside.com>
2022-12-21 14:21:45 -0500

    Implemented DatabaseCreation class, tests now create a test database

--- djarango/db/backends/arangodb/creation.py
+++ djarango/db/backends/arangodb/creation.py
@@ -1,13 +1,119 @@
 #
 # creation.py
 #
-# Timothy Graefe, Javamata LLC, Nov 2021
+# Adrian Garcia, December 2022
 #
+import sys
+
+from arango import DatabaseCreateError
 from django.db.backends.base.creation import BaseDatabaseCreation
 
 
-# Needed to link into backend options.
 class DatabaseCreation(BaseDatabaseCreation):
-    pass
+    def _quote_name(self, name):
+        return self.connection.ops.quote_name(name)
+
+    def _get_database_create_suffix(self, template):
+        # Pretty sure we don't need this.
+        pass
+
+    def _database_exists(self, cursor, database_name):
+        return cursor._conn.has_database(database_name)
+
+    def _execute_create_test_db(self, cursor, parameters, keepdb=False):
+        try:
+            if keepdb and self._database_exists(cursor, parameters["dbname"]):
+                # If the database should be kept and it already exists, don't
+                # try to create a new one.
+                return
+            else:
+                cursor._conn.create_database(parameters["dbname"])
+        except DatabaseCreateError as e:
+            if '[ERR 1207]' not in getattr(e, "message", ""):
+                # All errors except "database already exists" cancel tests.
+                self.log("Got an error creating the test database: %s" % e)
+                sys.exit(2)
+            elif not keepdb:
+                # If the database should be kept, ignore "database already
+                # exists".
+                raise
+
+    def _create_test_db(self, verbosity, autoclobber, keepdb=False):
+        """
+        Internal implementation - create the test db tables.
+        """
+        test_database_name = self._get_test_db_name()
+        test_db_params = {
+            "dbname": self.connection.ops.quote_name(test_database_name),
+            "suffix": self.sql_table_creation_suffix(),
+        }
+        # Create the test database and connect to it.
+        with self._nodb_cursor() as cursor:
+            try:
+                self._execute_create_test_db(cursor, test_db_params, keepdb)
+            except Exception as e:
+                # if we want to keep the db, then no need to do any of the below,
+                # just return and skip it all.
+                if keepdb:
+                    return test_database_name
+
+                self.log("Got an error creating the test database: %s" % e)
+                if not autoclobber:
+                    confirm = input(
+                        "Type 'yes' if you would like to try deleting the test "
+                        "database '%s', or 'no' to cancel: " % test_database_name
+                    )
+                if autoclobber or confirm == "yes":
+                    try:
+                        if verbosity >= 1:
+                            self.log(
+                                "Destroying old test database for alias %s..."
+                                % (
+                                    self._get_database_display_str(
+                                        verbosity, test_database_name
+                                    ),
+                                )
+                            )
+                        cursor._conn.delete_database(test_db_params["dbname"])
+                        self._execute_create_test_db(cursor, test_db_params, keepdb)
+                    except Exception as e:
+                        self.log("Got an error recreating the test database: %s" % e)
+                        sys.exit(2)
+                else:
+                    self.log("Tests cancelled.")
+                    sys.exit(1)
+
+        return test_database_name
+
+    def _clone_test_db(self, suffix, verbosity, keepdb=False):
+        # CREATE DATABASE ... WITH TEMPLATE ... requires closing connections
+        # to the template database.
+        self.connection.close()
+
+        source_database_name = self.connection.settings_dict["NAME"]
+        target_database_name = self.get_test_db_clone_settings(suffix)["NAME"]
+        test_db_params = {
+            "dbname": self._quote_name(target_database_name),
+            "suffix": self._get_database_create_suffix(template=source_database_name),
+        }
+        with self._nodb_cursor() as cursor:
+            try:
+                self._execute_create_test_db(cursor, test_db_params, keepdb)
+            except Exception:
+                try:
+                    if verbosity >= 1:
+                        self.log(
+                            "Destroying old test database for alias %s..."
+                            % (
+                                self._get_database_display_str(
+                                    verbosity, target_database_name
+                                ),
+                            )
+                        )
+                    cursor._conn.delete_database(test_db_params["dbname"])
+                    self._execute_create_test_db(cursor, test_db_params, keepdb)
+                except Exception as e:
+                    self.log("Got an error cloning the test database: %s" % e)
+                    sys.exit(1)
 
 # creation.py
--- djarango/db/backends/arangodb/schema.py
+++ djarango/db/backends/arangodb/schema.py
@@ -87,6 +87,16 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
             else:
                 logger.debug(f"Created graph {graph_name} with new edge definition")
 
+    def quote_value(self, value):
+        """
+        Return a quoted version of the value so it's safe to use in an SQL
+        string. This is not safe against injection from user code; it is
+        intended only for use in making SQL scripts or preparing default values
+        for particularly tricky backends (defaults are not user-defined, though,
+        so this is safe).
+        """
+        return '"%s"' % value
+
 
 """
 Fake ModelA info:
